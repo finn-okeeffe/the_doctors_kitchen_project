c8244ef8328a07c4a77610d2ca6a7db7444e5946
finn-okeeffe <okeeffefinn@gmail.com>
2023-07-29 17:50:54 +1200

    fixed url getter including non-recipe pages

--- scraper.py
+++ scraper.py
@@ -12,6 +12,12 @@ class TheDoctorsKitchenScraper():
 
     sitemap_url = "https://thedoctorskitchen.com/sitemap.xml"
     recipe_url_start = "https://thedoctorskitchen.com/recipes/"
+    url_starts_to_ignore = [
+        "https://thedoctorskitchen.com/recipes/breakfast/",
+        "https://thedoctorskitchen.com/recipes/lunch/",
+        "https://thedoctorskitchen.com/recipes/snack/",
+        "https://thedoctorskitchen.com/recipes/dinner/"
+    ]
     recipe_url_list = []
 
     def __init__(self):
@@ -22,8 +28,18 @@ class TheDoctorsKitchenScraper():
         # parses the sitemap to return a list of recipe urls, to be used by n_random_recipes and all_recipes
         sitemap_xml = requests.get(self.sitemap_url)
         sitemap_soup = BeautifulSoup(sitemap_xml.content, "xml")
-        self.recipe_url_list = [loc.text.strip() for loc in sitemap_soup.find_all("loc") if self.recipe_url_start in loc.text]
+        all_urls = [loc.text.strip() for loc in sitemap_soup.find_all("loc")]
+        for url in all_urls:
+            if self.is_recipe_page(url):
+                self.recipe_url_list.append(url)
         log.log(f"loaded {len(self.recipe_url_list)} recipe URLs")
+
+    def is_recipe_page(self, url: str) -> bool:
+        page_is_recipe = self.recipe_url_start in url
+        for address in self.url_starts_to_ignore:
+            page_is_recipe = page_is_recipe and not(address in url)
+        
+        return page_is_recipe
 
     def n_random_recipes(self, n: int) -> List[Recipe]:
         # returns n distinct random recipe objects from the doctors kitchen
@@ -42,6 +58,7 @@ if __name__ == "__main__":
     log.logging = True
     testScraper = TheDoctorsKitchenScraper()
     recipes = testScraper.n_random_recipes(5)
-    # for i,r in enumerate(recipes):
-    #     print(f"{i}) {r.title}")
+    
+    for i,r in enumerate(recipes):
+        print(f"{i}) {r.title} {r.url}")
     
