cba72991e30a27aaca731625782b028c7f59f9e4
finn-okeeffe <okeeffefinn@gmail.com>
2023-07-31 11:06:13 +1200

    update logs and added logging to file

--- insert_recipe.py
+++ insert_recipe.py
@@ -16,52 +16,38 @@ class RecipeInserter(Inserter):
 
     def insert_recipe(self, recipeObject: Recipe) -> int:
         ## Attempts to insert the given recipe, and returns an exit code; these are as follows:
-        # 1: successfully inserted to recipe table
-        # 2: all of exit code 1 and inserted in meal table
-        # 3: all of exit code 2 and inserted into ingredient table
+        # 1: successfully inserted to recipe table, check logs
+        # 2: all of exit code 1 and inserted in meal table, check logs
+        # 3: all of exit code 2 and inserted into ingredient table, check logs
         # 4: completed all insertions
-        # 0: did not complete any insertions
-        # -1: already in database
-        # -2: not implemented
+        # 0: did not complete any insertions, check logs
         log.log(f"Inserting recipe from url {recipeObject.url}")
         exit_code = 0
-        if self.recipe_in_db(recipeObject):
-            exit_code = -1
-        else:
-            try:
-                # insert to recipe table
-                log.log("Inserting into recipe table...")
-                self.insert_into_recipe_table(recipeObject)
-                exit_code = 1
+        try:
+            # insert to recipe table
+            log.log("Inserting into recipe table...")
+            self.insert_into_recipe_table(recipeObject)
+            exit_code = 1
 
-                # insert into meal table
-                log.log("Inserting into meal table...")
-                self.insert_into_meal_table(recipeObject)
-                exit_code = 2
-                
-                # insert to ingredient table
-                log.log("Inserting into ingredient table...")
-                self.insert_into_ingredient_table(recipeObject)
-                exit_code = 3
-                
-                # insert to recipe_equipment table
-                log.log("Inserting into recipe_equipment table...")
-                self.insert_into_recipe_equipment_table(recipeObject)
-                exit_code = 4
+            # insert into meal table
+            log.log("Inserting into meal table...")
+            self.insert_into_meal_table(recipeObject)
+            exit_code = 2
+            
+            # insert to ingredient table
+            log.log("Inserting into ingredient table...")
+            self.insert_into_ingredient_table(recipeObject)
+            exit_code = 3
+            
+            # insert to recipe_equipment table
+            log.log("Inserting into recipe_equipment table...")
+            self.insert_into_recipe_equipment_table(recipeObject)
+            exit_code = 4
 
-            except Exception as e:
-                log.log(f"Insertion of recipe at url {recipeObject.url} failed: {e}")
-        self.failed_recipes.append((recipeObject, exit_code))
+        except Exception as e:
+            log.log(f"ERROR: Insertion of recipe at url {recipeObject.url} failed: {e}")
+            self.failed_recipes.append((recipeObject, exit_code))
         return exit_code
-    
-
-
-    def recipe_in_db(self, recipeObject: Recipe) -> bool:
-        self.cur.execute("SELECT id FROM recipe WHERE url=%s",(recipeObject.url,))
-        results = self.cur.fetchall()
-        if len(results) != 0:
-            log.log(f"Insertion failed: already in database with recipe.id {results[0][0]} - url: {recipeObject.url}")
-            return False
 
 
 
@@ -79,7 +65,9 @@ def insert_recipes(recipes_list: List[Recipe]):
 
 if __name__ == "__main__":
     log.logging = True
-    log.log_progress
+    log.log_progress = True
+    log.to_file = True
+
     recipeFactory = RecipeFactory()
 
     test_urls = ["https://thedoctorskitchen.com/recipes/smoky-mushroom-and-tempeh-veggie-burgers/"]
--- log.py
+++ log.py
@@ -1,5 +1,29 @@
+from datetime import date, datetime
+
 logging = False
 log_progress = False
+to_file = True
+
+
+file = None
+log_filepath = f"log_{date.today().isoformat()}.log"
+
 def log(string: str):
     if logging:
-        print(string)
+        if to_file:
+            log_to_file(string)
+        else:
+            log_to_console(string)
+
+def log_string(string: str):
+    return f"{datetime.now().time()} || {string}\n"
+
+def log_to_file(string: str):
+    global file
+    if not(file):
+        file = open(log_filepath, 'a')
+        file.write(log_string("Loaded this log file"))
+    file.write(log_string(string))
+
+def log_to_console(string: str):
+    print(log_string(string), end="")
