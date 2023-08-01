970a96f95755ab1489003085aab2071b87dba135
finn-okeeffe <okeeffefinn@gmail.com>
2023-08-01 12:49:03 +1200

    update query to reset SERIAL id columns

--- sql_queries/drop_all_recipe_entries.sql
+++ sql_queries/drop_all_recipe_entries.sql
@@ -1,4 +1,8 @@
 DELETE FROM ingredient;
+ALTER SEQUENCE ingredient_id_seq RESTART WITH 1;
 DELETE FROM meal;
+ALTER SEQUENCE meal_id_seq RESTART WITH 1;
 DELETE FROM recipe_equipment;
+ALTER SEQUENCE recipe_equipment_id_seq RESTART WITH 1;
 DELETE FROM recipe;
+ALTER SEQUENCE recipe_id_seq RESTART WITH 1;
