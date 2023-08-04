292eb30d81b7a6ba595d796c9559adac57d57fac
finn-okeeffe <okeeffefinn@gmail.com>
2023-08-04 13:53:09 +1200

    fully functional

--- scraper/update_equipment_tables.py
+++ scraper/update_equipment_tables.py
@@ -4,14 +4,12 @@ import secret
 import psycopg2 as pg2
 import pandas as pd
 from typing import Set, List, Tuple
+import log
 
 
 new_equipment_synonyms = {
     "slow cooker": {"slow cooker", "crock pot"}
 }
-
-def equipment_in_db(ids: DatabaseIdFields, equipment_name: str) -> bool:
-    return equipment_name in ids.equipment_ids
 
 
 def methods_from_db(cur) -> pd.DataFrame:
@@ -29,8 +27,8 @@ def read_data_from_df(cur) -> Tuple[pd.DataFrame, pd.DataFrame]:
     ingredient_df = ingredients_from_db(cur)
     return method_df, ingredient_df
 
-def delete_old_equipment_data(cur):
-    cur.execute("DELETE FROM recipe_equipment;")
+def delete_old_recipe_equipment_data(cur):
+    cur.execute("DELETE FROM recipe_equipment RETURNING id;")
 
 def ingredient_strings_from_df(ingredients: pd.DataFrame) -> List[str]:
     return list(ingredients["quantity"].astype(str) + " " + ingredients["unit"].astype(str) + " " +\
@@ -55,17 +53,37 @@ def get_all_recipe_equipment(method_df: pd.DataFrame, ingredient_df: pd.DataFram
     return recipe_equipment
 
 def insert_recipe_equipment(recipe_equipment: pd.DataFrame, inserter: Inserter):
-    pass
+    for _,row in recipe_equipment.iterrows():
+        inserter.insert_into_recipe_equipment(row['recipe_id'], row['equipment_name'])
 
-def refresh_equipment_synonyms(cur):
+def refresh_equipment(conn, cur):
+    log.log('Re-parsing equipment in recipes')
+    log.log('Reading method and ingredients...')
     method_df, ingredient_df = read_data_from_df(cur)
-
-    delete_old_equipment_data(cur)
-
+    log.log('parsing new equipment...')
     recipe_equipment = get_all_recipe_equipment(method_df, ingredient_df)
-
+    log.log(f'{len(recipe_equipment)} recipe_equipment entries created')
+    log.log('deleting old recipe_equipment rows...')
+    delete_old_recipe_equipment_data(cur)
+    num_rows_deleted = len(cur.fetchall())
+    log.log(f'{num_rows_deleted} rows to be deleted from recipe_equipment')
+    log.log('creating inserter object...')
     inserter = Inserter(cur)
+    log.log('inserting new recipe_equipment rows...')
     insert_recipe_equipment(recipe_equipment, inserter)
+    log.log('getting confirmation of changes:')
+
+    print(f"Processing complete, changes will delete {num_rows_deleted} rows and insert {len(recipe_equipment)} rows in recipe_equipment")
+    user_input = None
+    while user_input not in ["y","n"]:
+        user_input = input("Do you with to commit the changes [y/n]: ").lower()
+    if user_input == "y":
+        log.log('Changes committed')
+        conn.commit()
+    else:
+        conn.rollback()
+        log.log('Changes rolled back')
+    log.log('done!')
     
 def comparison_test(cur):
     # compares locally parsed equipment to those stored in the db
@@ -80,7 +98,8 @@ def comparison_test(cur):
 def main():
     with pg2.connect(**secret.connection_kw_args) as conn:
         with conn.cursor() as cur:
-            comparison_test(cur)
+            refresh_equipment(conn, cur)
+            # comparison_test(cur)
 
 
 if __name__ == "__main__":
